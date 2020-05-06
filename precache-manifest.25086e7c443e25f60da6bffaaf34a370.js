9df34cef4b9372461b1f2939090741c2857b9d92
mueksh <mukeshbsws@gmail.com>
2020-05-06 03:27:17 +0300

    Updates

---
+++ precache-manifest.25086e7c443e25f60da6bffaaf34a370.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b7d441e31d88bc7967eb76b75123d2",
+    "url": "/slot-machine-demo/index.html"
+  },
+  {
+    "revision": "b7ec362df34871645fc2",
+    "url": "/slot-machine-demo/static/css/main.de948ea3.chunk.css"
+  },
+  {
+    "revision": "3ab39eab2013d98c0816",
+    "url": "/slot-machine-demo/static/js/2.6ad577eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/slot-machine-demo/static/js/2.6ad577eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ec362df34871645fc2",
+    "url": "/slot-machine-demo/static/js/main.5720a1f4.chunk.js"
+  },
+  {
+    "revision": "31fcdc68d91ee6f9c3e8",
+    "url": "/slot-machine-demo/static/js/runtime-main.d7b2671d.js"
+  },
+  {
+    "revision": "565a57790e190fec06a5793e260d809f",
+    "url": "/slot-machine-demo/static/media/Cherry.565a5779.png"
+  },
+  {
+    "revision": "2a9912fbae8eda320fcf544a9d145abf",
+    "url": "/slot-machine-demo/static/media/body.2a9912fb.ttf"
+  }
+]);
